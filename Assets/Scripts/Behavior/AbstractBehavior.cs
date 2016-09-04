1bd59456e3d004d2d21e6a1c6e42997bfedb3c33
Stephanie Allen <Stephanie Allen>
2016-09-04 14:26:10 -0700

    intial commit

--- Assets/Scripts/Behavior/AbstractBehavior.cs
+++ Assets/Scripts/Behavior/AbstractBehavior.cs
@@ -1,31 +1,24 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public abstract class AbstractBehavior : MonoBehaviour {
+public class AbstractBehavior : MonoBehaviour {
 
 	public Buttons[] inputButtons;
+	public MonoBehaviour[] disabledScripts;
 
-	//use protected access so outside classes do not have access, only those which 
-	//extend this class
 	protected InputState inputState;
 	protected Rigidbody2D body2d;
-	//protected CollisionState collisionState;
+	protected CollisionState collisionState;
 
-	//Awake() is called before Start().  Mark as virtual so 
-	//classes extending this class can override this method.
 	protected virtual void Awake (){
 		inputState = GetComponent<InputState> ();
 		body2d = GetComponent<Rigidbody2D> ();
-		//collisionState = GetComponent<CollisionState> ();
+		collisionState = GetComponent<CollisionState> ();
 	}
 
-	// Use this for initialization
-	void Start () {
-
-	}
-
-	// Update is called once per frame
-	void Update () {
+	protected virtual void ToggleScripts(bool value){
+		foreach (var script in disabledScripts)
+			script.enabled = value;
 
 	}
 }
--- Assets/Scripts/Managers/PlayerManager.cs
+++ Assets/Scripts/Managers/PlayerManager.cs
@@ -8,28 +8,39 @@ using System.Collections;
 public class PlayerManager : MonoBehaviour {
 
 	private InputState inputState;
-	//private Walk walkBehavior;
+	private Walk walkBehavior;
 	private Animator animator;
+	private CollisionState collisionState;
+
 
 	void Awake () {
 		inputState = GetComponent<InputState> ();
-		//walkBehavior = GetComponent<Walk> ();
+		walkBehavior = GetComponent<Walk> ();
 		animator = GetComponent<Animator> ();
+		collisionState = GetComponent<CollisionState> ();
+
 	}
 
 	// Use this for initialization
 	void Start () {
 	
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
-		if (inputState.absVelX == 0) {
+		//change to idle state
+		if (collisionState.standing) {
 			ChangeAnimationState (0);
 		}
+		//change to walk state
 		if (inputState.absVelX > 0) {
 			ChangeAnimationState (1);
 		}
+		//change to jump state
+		if (inputState.absVelY > 0) {
+			ChangeAnimationState (2);
+		}
+
 
 	}
 
